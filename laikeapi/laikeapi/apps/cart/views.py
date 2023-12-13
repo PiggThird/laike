abd74945a77b50842dc95db62c010ae72cec0fb9
piggthird <fanchenwei2002@163.com>
2023-12-13 20:55:35 +0800

    服务端实现商品的全选与全不选的勾选状态切换

--- laikeapi/laikeapi/apps/cart/views.py
+++ laikeapi/laikeapi/apps/cart/views.py
@@ -95,3 +95,34 @@ class CartAPIView(APIView):
 
         redis.hset(f"cart_{user_id}", course_id, selected)
         return Response({"errmsg": "ok"})
+
+    def put(self, request):
+        """"全选 / 全不选"""
+        user_id = request.user.id
+        selected = int(bool(request.data.get("selected", True)))
+        redis = get_redis_connection("cart")
+
+        # 获取购物车中所有商品课程信息
+        cart_hash = redis.hgetall(f"cart_{user_id}")
+        """
+        cart_hash = {
+            # b'商品课程ID': b'勾选状态', 
+            b'2': b'1', 
+            b'4': b'1', 
+            b'5': b'1'
+        }
+        """
+        if len(cart_hash) < 1:
+            return Response({"errmsg": "购物车没有任何商品。"}, status=status.HTTP_204_NO_CONTENT)
+
+        # 把redis中的购物车课程ID信息转换成普通列表
+        cart_list = [int(course_id.decode()) for course_id in cart_hash]
+
+        # 批量修改购物车中素有商品课程的勾选状态
+        pipe = redis.pipeline()
+        pipe.multi()
+        for course_id in cart_list:
+            pipe.hset(f"cart_{user_id}", course_id, selected)
+        pipe.execute()
+
+        return Response({"errmsg": "ok"})
